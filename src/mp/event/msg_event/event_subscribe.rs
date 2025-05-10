e607eb74ff5490fcef9d43adfe9b7504ea82be5d
Yoruno <xusir92@gmail.com>
2025-05-10 15:16:28 +0800

    fix: 解决订阅空event_key

--- src/mp/event/msg_event/event_subscribe.rs
+++ src/mp/event/msg_event/event_subscribe.rs
@@ -15,9 +15,11 @@ impl ReceivedMessageParser for SubScribeEvent {
         let ekn = node.descendants().find(|n| n.has_tag_name("EventKey"));
         let event = match ekn {
             Some(n) => {
-                let event_key = n.text().ok_or_else(|| {
+                let event_key = n.text().unwrap_or("");
+                if event_key.is_empty() {
                     return EventMessage::Subscribe;
-                })?;
+                }
+
                 let ticket = get_text_from_root(node, "Ticket")?;
                 EventMessage::SubscribeScan(ScanEvent {
                     event_key: event_key.to_string(),
